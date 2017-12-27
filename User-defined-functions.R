36401ea4dbd4025f8ba69d1be96690b1c5f7a7dd
rahulbwn <rahulbwn@users.noreply.github.com>
2017-12-27 12:11:53 -0600

    Create User-defined-functions.R

---
+++ User-defined-functions.R
@@ -0,0 +1,21 @@
+#Functions:
+
+testFunction<-function(a,b,c)
+{
+        value<- a*b
+        value<-value+c
+        print(value)
+}
+
+testFunction(2,3,4)
+testFunction(5,6,8)
+
+# non paramersized function
+
+test<-function()
+{
+    print("--------------------------------------------------------")
+    print("********************************************************")
+}
+
+test()
